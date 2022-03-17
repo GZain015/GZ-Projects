3d4afdac8643c5ec22821b423710f2097fd3ede2
GZain015 <gzain2001@gamil.com>
2022-03-17 12:19:12 +0500

    I am going to activate github

---
+++ src/gitprac/GZ.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gitprac;
+
+/**
+ *
+ * @author Ghulam Zain
+ */
+public class GZ {
+    [user]
+    Name=GZain015
+    email=gzain2001@gmail.com
+    
+}
